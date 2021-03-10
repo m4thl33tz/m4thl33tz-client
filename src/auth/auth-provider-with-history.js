f42370032e62dddac66fdccc0cc84b3fcc5e056d
Sam Formichella <43454748+sformichella@users.noreply.github.com>
2021-03-09 22:24:08 -0800

    put env back in Auth0 zero. Whoops! (#23)

--- src/auth/auth-provider-with-history.js
+++ src/auth/auth-provider-with-history.js
@@ -3,8 +3,8 @@ import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = 'dev-lmqx0j0a.us.auth0.com';
-  const clientId = 'lWtM6Z0UE2gehkEUjYa7ioI1uQgnnF2y';
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const history = useHistory();
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
